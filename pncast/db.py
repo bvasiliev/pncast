50bde1131aa1be54a03ab5eaa6a0766d59d90ed6
Bogdan Vasiliev <bogdan.vasiliev@gmail.com>
2016-12-02 16:19:03 +0500

    OC database creds

--- pncast/db.py
+++ pncast/db.py
@@ -5,7 +5,7 @@ from peewee import *
 from playhouse.postgres_ext import *
 from os import environ as env
 
-postgres = PostgresqlExtDatabase(env['POSTGRESQL_DATABASE'], 
+postgres = PostgresqlExtDatabase('postnauka', 
 	user = env['POSTGRESQL_USER'],
 	password = env['POSTGRESQL_PASSWORD'],
 	host = env['POSTGRESQL_SERVICE_HOST'] 
